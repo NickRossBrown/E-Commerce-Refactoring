ffb5cc4420660136dece1dec664b01660ee93f86
Nick Brown <nickrossbrown@gmail.com>
2018-09-21 13:19:16 -0700

    Add a button with ajax to show product details

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,14 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+    
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
