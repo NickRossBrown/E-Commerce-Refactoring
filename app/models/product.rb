ce67d6e371cf05a031283d85202d271e369b9fe6
Nick Brown <nickrossbrown@gmail.com>
2018-09-21 20:01:58 -0700

    add four validations for products with 4 corresponding tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :order_items
   
+  validates :name, :presence => true
+  validates :description, :presence => true
+  validates :price, :presence => true
+
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
  describe Product do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :price }
+    it { should have_many :order_items }
 
 end
